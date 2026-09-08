00c938250e1e1f7e0998e74fe7ceeceac33a9961
infonextgensolution1-cloud <info.nextgensolution1@gmail.com>
2026-09-09 00:04:55 +0200

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabaseClient.ts'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
